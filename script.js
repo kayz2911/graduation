a04c401681a20c76b631387794ddfa465ab5ce71
Do Hieu <dohuyhieu2911@gmail.com>
2023-10-04 00:07:28 +0700

    update

--- script.js
+++ script.js
@@ -1,73 +1,49 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+$(document).ready(function () {
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "HUST",
+      "Trường đại học Bách Khoa Hà Nội",
+      "Ha Noi University of Science and Technology",
+    ],
+    typeSpeed: 130,
+    backSpeed: 90,
+    loop: true,
+  });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+  let birthday = "Oct 21, 2023 16:00:00",
+    countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      let now = new Date().getTime(),
+        distance = countDown - now;
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+      //seconds
+    }, 0);
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["HUST", "Bách Khoa", "Trường Rác", "Trường Số 1 Đại Cồ Việt"],
-        typeSpeed: 130,
-        backSpeed: 90,
-        loop: true
-    });
+  const scrollToBottomBtn = document.getElementById("scrollToMap");
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  scrollToBottomBtn.addEventListener("click", function () {
+    const pageHeight = document.documentElement.scrollHeight;
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
+    // Scroll to the bottom of the page
+    window.scrollTo({
+      top: pageHeight,
+      behavior: "smooth",
     });
+  });
 });
